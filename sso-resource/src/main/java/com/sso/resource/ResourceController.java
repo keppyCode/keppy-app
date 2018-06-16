decac6cf95da56fe4e910c21befc1295db4403eb
keppyCode <935364745@qq.com>
2018-06-16 17:02:23 +0800

    添加sso-webtoken-server模块

---
+++ sso-resource/src/main/java/com/sso/resource/ResourceController.java
@@ -0,0 +1,28 @@
+package com.sso.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+@Controller
+public class ResourceController {
+
+    private static final String jwtTokenCookieName = "JWT-TOKEN";
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/protected-resource";
+    }
+
+    @RequestMapping("/protected-resource")
+    public String protectedResource() {
+        return "protected-resource";
+    }
+
+
+    @RequestMapping("/index")
+    public String index(HttpServletResponse httpServletResponse) {
+
+        return "index";
+    }
+}
